d31a4a95f52d81799cec79a38bd97903ac3d8dd4
iruka <iruka@psp.dip.jp>
2016-02-26 19:51:43 +0900

    add config,led,serial1,fifo,util

---
+++ pic32mx/util.h
@@ -0,0 +1,15 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+void	led_flip();
+void	led_test();
+void	led_blink();
+
+/*
+void	wait_ms(int ms);
+void	delay_us(int us);
+void	wait_nloop(int n);
+*/
+
+#endif
+
